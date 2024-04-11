5e933f5d73dfebb968894631a43a232ca3405473
Alex Sirota <alex.sirota@icloud.com>
2024-04-10 17:11:33 -0700

    Add swerve drive command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,14 @@ package frc.robot;
 
 import java.io.File;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
@@ -21,11 +24,21 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureBindings();
+
+    DriveCommand driveCmd = new DriveCommand(
+      swerve,
+      () -> MathUtil.applyDeadband(-driverXbox.getLeftY(), 0.02), // Y axis on joystick is X axis for FRC. Forward is postive-Y, so need to invert sign
+      () -> MathUtil.applyDeadband(-driverXbox.getLeftX(), 0.02), // X axis on joystick is Y axis for FRC. Left is positive-X, so need to invert sign
+      () -> MathUtil.applyDeadband(-driverXbox.getRightX(), 0.08)
+      ); // Rotation for FRC is CCW-positive, so need to invert sign
+      swerve.setDefaultCommand(driveCmd);
   }
 
   private void configureBindings() {
     driverXbox.start().onTrue((new InstantCommand(swerve::zeroGyro)));
 
+    // Reset pose-estimation when starting auton
+    RobotModeTriggers.autonomous().onTrue(new InstantCommand(() -> {swerve.resetGyroTo(swerve.getPose().getRotation());}));
   }
 
   public Command getAutonomousCommand() {
