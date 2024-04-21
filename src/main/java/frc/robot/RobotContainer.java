0434a0611ae4efa2c5ff4a80579fd82a0d7eb71d
Alex Sirota <alex.sirota@icloud.com>
2024-04-20 20:44:42 -0400

    Add explanatory comments and auton logic

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,12 +10,14 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import frc.robot.subsystems.SwerveSubsystem;
 import java.io.File;
 
+/**
+ * This class is where almost all of the robot is defined - logic and subsystems are all set up here.
+ */
 public class RobotContainer {
 
   SwerveSubsystem swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
@@ -24,7 +26,7 @@ public class RobotContainer {
   private final SendableChooser<Command> autoChooser;
 
   public RobotContainer() {
-    configureBindings();
+    configureTriggers();
 
     swerve.setDefaultCommand(
         swerve.driveCommand(
@@ -40,21 +42,32 @@ public class RobotContainer {
             // invert sign
             () ->
                 MathUtil.applyDeadband(
-                    -driverXbox.getRightX(),
-                    0.08))); // Rotation for FRC is CCW-positive, so need to invert sign));
-
+                    -driverXbox.getRightX(), // Rotation for FRC is CCW-positive, so need to invert sign
+                    0.08)
+          )
+      ); 
+    // Initialize autonomous chooser
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auton Path", autoChooser);
   }
 
-  private void configureBindings() {
+  /**
+   * This method is where all of the robot's logic is defined.
+   * We link {@link edu.wpi.first.wpilibj2.command.button.Trigger}s, such as controller buttons and 
+   * subsystem state, to {@link edu.wpi.first.wpilibj2.command.Command} instances.
+   * The advantage of configuring all the robot's logic here is that it's easy to find, and therefore
+   * easy to modify, what the robot does when something happens and why.
+   */
+  private void configureTriggers() {
+    // Controls
     driverXbox.start().onTrue(swerve.zeroYawCommand());
 
-    // Reset pose-estimation when starting auton
+    // Logic
+    // Reset pose estimation when auton starts
     RobotModeTriggers.autonomous().onTrue(swerve.resetGyroCommand());
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
