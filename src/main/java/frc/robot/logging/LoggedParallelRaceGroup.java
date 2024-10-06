6cc23751ee092c6fc0cd96a36e2c0e83ddf45243
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-10-06 14:14:22 -0700

    Setup Logger

--- src/main/java/frc/robot/logging/LoggedParallelRaceGroup.java
+++ src/main/java/frc/robot/logging/LoggedParallelRaceGroup.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class LoggedParallelRaceGroup extends Command {
